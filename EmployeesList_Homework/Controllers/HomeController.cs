50ffb9238491750f3a32d115753b1d2993808533
Serg <serg@example.com>
2020-11-26 00:48:43 +0300

    Реализована возможность вывода списка сотрудников

--- EmployeesList_Homework/Controllers/HomeController.cs
+++ EmployeesList_Homework/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeesList_Homework.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,19 @@ namespace EmployeesList_Homework.Controllers
 {
     public class HomeController : Controller
     {
+        private static IEnumerable<Employee> _employees = new List<Employee>() {
+            new Employee() { Id = 1, Name = "Денис", Surename = "Огурцов", Patronymic = "Сергеевич", Gender = "Мужской", Age = 38, Profession = "Электрормонтёр"},
+            new Employee() { Id = 2, Name = "Ольга", Surename = "Самохвалова", Patronymic = "Викторовна", Gender = "Женский", Age = 32, Profession = "Бухгалтер"},
+            new Employee() { Id = 3, Name = "Виктор", Surename = "Быков", Patronymic = "Николаевич", Gender = "Мужской", Age = 55, Profession = "Мастер"},
+            new Employee() { Id = 4, Name = "Олег", Surename = "Костерин", Patronymic = "Викторович", Gender = "Мужской", Age = 38, Profession = "Инженер АСУ ТП"},
+            new Employee() { Id = 5, Name = "Юрий", Surename = "Корюнин", Patronymic = "Анатольевич", Gender = "Мужской", Age = 46, Profession = "Слесарь"},
+            new Employee() { Id = 6, Name = "Анна", Surename = "Огурцова", Patronymic = "Александровна", Gender = "Женский", Age = 35, Profession = "Инженер по ТБ"},
+        };
+
+        
         public IActionResult EmployeesList()
         {
-            return View();
+            return View(_employees);
         }
     }
 }
---
+++ EmployeesList_Homework/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesList_Homework.Models
+{
+    /// <summary>Сотрудник</summary>
+    public class Employee
+    {
+        /// <summary>Идентификатор</summary>
+        public int Id { get; set; }
+
+        /// <summary>Имя</summary>
+        public string Name { get; set; }
+
+        /// <summary>Фамилия</summary>
+        public string Surename { get; set; }
+
+        /// <summary>Отчество</summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>Пол</summary>
+        public string Gender { get; set; }
+
+        /// <summary>Возраст</summary>
+        public int Age { get; set; }
+
+        /// <summary>Профессия</summary>
+        public string Profession { get; set; }
+    }
+}
